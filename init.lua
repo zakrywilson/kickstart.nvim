2136a134a88d491f9342cf26265fc801e758c138
Zach Wilson <zakrywilson@gmail.com>
2023-12-28 12:46:26 -0600

    Only useg HTML for HTML

--- init.lua
+++ init.lua
@@ -516,7 +516,7 @@ local servers = {
   -- pyright = {},
   -- rust_analyzer = {},
   -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+  html = { filetypes = { 'html' } },
 
   lua_ls = {
     Lua = {
