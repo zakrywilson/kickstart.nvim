c3e3a88743590e42a29ca2180d427e78b945667d
Zach Wilson <zakrywilson@gmail.com>
2023-12-28 19:40:32 -0600

    Adds noremap to Ctrl-S for saving

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -2,4 +2,4 @@
 vim.keymap.set('n', '<leader>a', '<esc>GVgg', { noremap = true, silent = true })
 
 -- Save
-vim.keymap.set('n', "<C-s>", "<cmd>w<cr>", { silent = true })
+vim.keymap.set('n', "<C-s>", "<cmd>w<cr>", { noremap = true, silent = true })
