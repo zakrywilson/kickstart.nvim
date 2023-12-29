10f53ce78fd8934ef89f3ea3dc2c54b71be5bb6b
Zach Wilson <zakrywilson@gmail.com>
2023-12-28 19:40:50 -0600

    Adds <Leader>Y system clipboard copies

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -3,3 +3,8 @@ vim.keymap.set('n', '<leader>a', '<esc>GVgg', { noremap = true, silent = true })
 
 -- Save
 vim.keymap.set('n', "<C-s>", "<cmd>w<cr>", { noremap = true, silent = true })
+
+-- Copy to system clipboard
+vim.keymap.set({'n', 'v'}, "<leader>y", '"+y', { silent = true }) -- entire line
+vim.keymap.set('n', "<leader>Y", 'v$h"+y', { silent = true })     -- to end of line (normal)
+vim.keymap.set('v', "<leader>Y", '$h"+y', { silent = true })      -- to end of line (visual)
