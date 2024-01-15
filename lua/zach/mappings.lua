17be3a9ed9df56b041db491b52adc6bd7086e5ba
Zach Wilson <zakrywilson@gmail.com>
2024-01-15 12:10:40 -0600

    Fixes formatting

--- lua/zach/mappings.lua
+++ lua/zach/mappings.lua
@@ -8,7 +8,7 @@ vim.keymap.set('n', '<leader>a', '<esc>GVgg', { noremap = true, silent = true })
 vim.keymap.set('n', "<C-s>", "<cmd>w<cr>", { noremap = true, silent = true })
 
 -- Copy to system clipboard
-vim.keymap.set({'n', 'v'}, "<leader>y", [["+y]], { silent = true }) -- entire line
+vim.keymap.set({ 'n', 'v' }, "<leader>y", [["+y]], { silent = true }) -- entire line
 vim.keymap.set('n', "<leader>Y", [[v$h"+y]], { silent = true })     -- to end of line (normal)
 vim.keymap.set('v', "<leader>Y", [[$h"+y]], { silent = true })      -- to end of line (visual)
 
@@ -16,4 +16,4 @@ vim.keymap.set('v', "<leader>Y", [[$h"+y]], { silent = true })      -- to end of
 vim.keymap.set('x', "<leader>p", [["_dP]])
 
 -- Delete without overwriting register
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
