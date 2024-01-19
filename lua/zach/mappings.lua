b99e78534bb2aff1eabaaf75ff7bfca5aefd81c7
Zach Wilson <zakrywilson@gmail.com>
2024-01-18 19:24:25 -0600

    Makes formatting consistent in mappings

--- lua/zach/mappings.lua
+++ lua/zach/mappings.lua
@@ -1,19 +1,19 @@
 -- Esc disables highlighting
-vim.keymap.set('n', '<esc>', '<cmd>noh<cr>')
+vim.keymap.set('n', '<Esc>', '<Cmd>noh<CR>')
 
 -- Select all
-vim.keymap.set('n', '<leader>a', '<esc>GVgg', { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>a', '<Esc>GVgg', { noremap = true, silent = true })
 
 -- Save
-vim.keymap.set('n', "<C-s>", "<cmd>w<cr>", { noremap = true, silent = true })
+vim.keymap.set('n', "<C-s>", "<Cmd>w<CR>", { noremap = true, silent = true })
 
 -- Copy to system clipboard
-vim.keymap.set({ 'n', 'v' }, "<leader>y", [["+y]], { silent = true }) -- entire line
-vim.keymap.set('n', "<leader>Y", [[v$h"+y]], { silent = true })     -- to end of line (normal)
-vim.keymap.set('v', "<leader>Y", [[$h"+y]], { silent = true })      -- to end of line (visual)
+vim.keymap.set({ 'n', 'v' }, "<Leader>y", [["+y]], { silent = true }) -- entire line
+vim.keymap.set('n', "<Leader>Y", [[v$h"+y]], { silent = true })       -- to end of line (normal)
+vim.keymap.set('v', "<Leader>Y", [[$h"+y]], { silent = true })        -- to end of line (visual)
 
 -- Paste without overwriting register 0
-vim.keymap.set('x', "<leader>p", [["_dP]])
+vim.keymap.set('x', "<Leader>p", [["_dP]])
 
 -- Delete without overwriting register
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+vim.keymap.set({ 'n', 'v' }, '<Leader>d', [["_d]])
