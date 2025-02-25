92b24d09126654ba46556477661f41e4d0b90bc0
Zach Wilson <zakrywilson@gmail.com>
2025-02-24 20:24:17 -0600

    Adds undo and swap directories

--- lua/zach/options.lua
+++ lua/zach/options.lua
@@ -5,3 +5,8 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.softtabstop = 4
+
+-- Undo and Swap file
+vim.o.undofile = true
+vim.o.undodir = vim.fn.stdpath("config") .. "/undo"
+vim.o.directory = vim.fn.stdpath("config") .. "/swap"
