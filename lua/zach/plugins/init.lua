848387a79c2f5341ee2a5f6fd08a2f34ff3ded4c
Zach Wilson <zakrywilson@gmail.com>
2025-03-09 18:10:59 -0500

    Adds render-markdown plugin

--- lua/zach/plugins/init.lua
+++ lua/zach/plugins/init.lua
@@ -35,6 +35,21 @@ return {
     dependencies = "nfrid/treesitter-utils",
   },
   {
+    "MeanderingProgrammer/render-markdown.nvim",
+    -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      code = {
+        sign = false,
+        style = "full",
+        language_name = false,
+      },
+    },
+  },
+  {
     dir = "~/Development/tmsync/extra/nvim/tmsync.nvim",
     config = function()
       require("tmsync").setup({
