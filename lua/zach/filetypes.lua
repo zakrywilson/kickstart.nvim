f28b954a91afdbc0e9b2addd15524737f190426d
Zach Wilson <zakrywilson@gmail.com>
2025-06-15 15:33:45 -0500

    Adds scheme filetype

--- lua/zach/filetypes.lua
+++ lua/zach/filetypes.lua
@@ -3,3 +3,8 @@ vim.filetype.add({
     templ = "templ", -- required for vrischmann/tree-sitter-templ
   },
 })
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.scm",
+  command = "set filetype=scheme",
+})
