58bcb5be168c91a25dd67f11fb8799ae6f76bd1e
Zach Wilson <zakrywilson@gmail.com>
2025-08-30 14:43:27 -0500

    Auto Session

--- lua/zach/plugins/init.lua
+++ lua/zach/plugins/init.lua
@@ -105,6 +105,18 @@ return {
     end,
   },
   {
+    "rmagatti/auto-session",
+    lazy = false,
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      -- log_level = 'debug',
+    },
+  },
+  {
     "Olical/conjure",
     ft = { "clojure", "fennel", "janet", "racket", "scheme", "lua" },
     config = function()
